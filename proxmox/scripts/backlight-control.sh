ac352c538e3cca4693b761b8bc4a90c8be2262c3
ej8899 <ej8899@gmail.com>
2025-08-29 20:59:27 -0600

    add: laptop backlight controller

---
+++ proxmox/scripts/backlight-control.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check for the correct number of arguments
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 {on|off}"
+    exit 1
+fi
+
+# Process the input argument to decide which command to run
+if [ "$1" == "on" ]; then
+    echo 0 | sudo tee /sys/class/backlight/*/bl_power
+    echo "Screen turned ON."
+elif [ "$1" == "off" ]; then
+    echo 1 | sudo tee /sys/class/backlight/*/bl_power
+    echo "Screen turned OFF."
+else
+    echo "Invalid argument. Usage: $0 {on|off}"
+    exit 1
+fi
