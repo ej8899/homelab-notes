cededcff7e46afa731ff949b72007048ff752af1
ej8899 <ej8899@gmail.com>
2025-08-29 21:07:08 -0600

    update: tweak backlight control code (assume sudo)

--- proxmox/scripts/backlight-control.sh
+++ proxmox/scripts/backlight-control.sh
@@ -8,10 +8,10 @@ fi
 
 # Process the input argument to decide which command to run
 if [ "$1" == "on" ]; then
-    echo 0 | sudo tee /sys/class/backlight/*/bl_power
+    echo 0 | tee /sys/class/backlight/*/bl_power
     echo "Screen turned ON."
 elif [ "$1" == "off" ]; then
-    echo 1 | sudo tee /sys/class/backlight/*/bl_power
+    echo 1 | tee /sys/class/backlight/*/bl_power
     echo "Screen turned OFF."
 else
     echo "Invalid argument. Usage: $0 {on|off}"
